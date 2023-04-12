e19d38ff83422455249ed7eeae1e2fcd619690b9
Lazar <lazargb@alumnos.iesgalileo.esgit config --global user.name lazargb@alumnos.iesgalileo.esgit config --global user.email lazargb@alumnos.iesgalileo.esgit config --global user.email lazargb@alumnos.iesgalileo.es>
2023-04-12 20:29:29 +0200

    v2: * AlumnosGrupo.java *Programa.java (modificado)probar Grupo

--- Programa.java
+++ Programa.java
@@ -1,9 +1,14 @@
 class Programa
 {
 public static void main (String [] args){
+    Alumnos a1=new Alumnos(1,"pepe");
+    System.out.println("Nº de matricula ="+a1.getNm());
+    System.out.println("Nombre="+a1.getNombre());
+    System.out.println(a1);  
+    
+/*
+ este texto desapareceraaa
+ */
 
-
-
-
-}
+ }
 }
